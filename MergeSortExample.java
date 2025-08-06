3d19e92dbb1c5ff0e6063c211074e0228825f450
abhaybarai05 <abhay.2403184101@vcet.edu.in>
2025-08-06 09:48:00 +0530

    Add files via upload

---
+++ MergeSortExample.java
@@ -0,0 +1,61 @@
+
+import java.util.Scanner;
+
+public class MergeSortExample {
+
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    public static void merge(int[] arr, int left, int mid, int right) {
+        int[] temp = new int[right - left + 1];
+        int i = left, j = mid + 1, k = 0;
+
+        while (i <= mid && j <= right) {
+            temp[k++] = (arr[i] <= arr[j]) ? arr[i++] : arr[j++];
+        }
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+        while (j <= right) {
+            temp[k++] = arr[j++];
+        }
+        for (int m = 0; m < temp.length; m++) {
+            arr[left + m] = temp[m];
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Before sorting:");
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+
+        mergeSort(arr, 0, arr.length - 1);
+
+        System.out.println("After sorting:");
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+
+        sc.close();
+    }
+}
